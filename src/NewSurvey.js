c3ef806d258353e47b8f450f89aad98cc23cee39
Yiming <759832901@qq.com>
2019-06-24 14:59:54 +0100

    PHQ9

--- src/NewSurvey.js
+++ src/NewSurvey.js
@@ -50,205 +50,107 @@ class NewSurvey extends Component {
         elements: [
           {
             "type": "radiogroup",
-            "name": "position",
-            "title": "Choose job position...",
+            "name": "Question1",
+            "title": "Little interest or pleasure in doing things",
             "isRequired": true,
             "colCount": 0,
-            "choices": ["1|Designer", "2|Front-end Developer", "3|Back-end Developer", "4|Database Administrator", "5|System Engineer"]
-          },
-          {
-            type: "barrating",
-            name: "barrating1",
-            ratingTheme: "css-stars",
-            title: "Please rate the movie you've just watched",
-            choices: ["1", "2", "3", "4", "5"]
-          },
-          {
-            type: "imagepicker",
-            name: "choosepicture",
-            title: "What animal would you like to see first ?",
-            choices: [
-              {
-                value: "lion",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
-              },
-              {
-                value: "giraffe",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
-              },
-              {
-                value: "panda",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
-              },
-              {
-                value: "camel",
-                imageLink:
-                  "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
-              }
-            ]
-          },
-          {
-            type: "bootstrapslider",
-            name: "bootstrapslider"
-          },
-          {
-            type: "dropdown",
-            renderAs: "select2",
-            choicesByUrl: {
-              url: "https://restcountries.eu/rest/v1/all"
-            },
-            name: "countries",
-            title: "Please select the country you have arrived from:"
-          },
-          {
-            type: "signaturepad",
-            name: "sign",
-            title: "Please enter your signature"
-          },
-          {
-            type: "sortablelist",
-            name: "lifepriopity",
-            title: "Life Priorities ",
-            isRequired: true,
-            colCount: 0,
-            choices: ["family", "work", "pets", "travels", "games"]
-          },
-          {
-            name: "date",
-            type: "datepicker",
-            inputType: "date",
-            title: "Your favorite date:",
-            dateFormat: "mm/dd/yy",
-            isRequired: true
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
           }
         ]
       },
       {
         questions: [
           {
-            type: "signaturepad",
-            width: "500px",
-            name: "Signature Pad - you can enter your signature here:"
-          },
-          {
-            type: "matrix",
-            name: "Quality",
-            title:
-              "Please indicate if you agree or disagree with the following statements",
-            columns: [
-              {
-                value: 1,
-                text: "Strongly Disagree"
-              },
-              {
-                value: 2,
-                text: "Disagree"
-              },
-              {
-                value: 3,
-                text: "Neutral"
-              },
-              {
-                value: 4,
-                text: "Agree"
-              },
-              {
-                value: 5,
-                text: "Strongly Agree"
-              }
-            ],
-            rows: [
-              {
-                value: "affordable",
-                text: "Product is affordable"
-              },
-              {
-                value: "does what it claims",
-                text: "Product does what it claims"
-              },
-              {
-                value: "better then others",
-                text: "Product is better than other products on the market"
-              },
-              {
-                value: "easy to use",
-                text: "Product is easy to use"
-              }
-            ]
-          },
-          {
-            type: "rating",
-            name: "satisfaction",
-            title: "How satisfied are you with the Product?",
-            mininumRateDescription: "Not Satisfied",
-            maximumRateDescription: "Completely satisfied"
-          },
-          {
-            type: "rating",
-            name: "recommend friends",
-            visibleIf: "{satisfaction} > 3",
-            title:
-              "How likely are you to recommend the Product to a friend or co-worker?",
-            mininumRateDescription: "Will not recommend",
-            maximumRateDescription: "I will recommend"
-          },
-          {
-            type: "comment",
-            name: "suggestions",
-            title: "What would make you more satisfied with the Product?"
+            "type": "radiogroup",
+            "name": "Question2",
+            "title": "Feeling down, depressed, or hopeless",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
           }
         ]
       },
       {
         questions: [
           {
-            type: "radiogroup",
-            name: "price to competitors",
-            title: "Compared to our competitors, do you feel the Product is",
-            choices: [
-              "Less expensive",
-              "Priced about the same",
-              "More expensive",
-              "Not sure"
-            ]
-          },
-          {
-            type: "radiogroup",
-            name: "price",
-            title: "Do you feel our current price is merited by our product?",
-            choices: [
-              "correct|Yes, the price is about right",
-              "low|No, the price is too low for your product",
-              "high|No, the price is too high for your product"
-            ]
-          },
-          {
-            type: "multipletext",
-            name: "pricelimit",
-            title: "What is the... ",
-            items: [
-              {
-                name: "mostamount",
-                title: "Most amount you would every pay for a product like ours"
-              },
-              {
-                name: "leastamount",
-                title: "The least amount you would feel comfortable paying"
-              }
-            ]
+            "type": "radiogroup",
+            "name": "Question3",
+            "title": "Trouble falling or staying asleep, or sleeping much.",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
           }
         ]
       },
       {
         questions: [
           {
-            type: "text",
-            name: "email",
-            title:
-              'Thank you for taking our survey. Please enter your email address, then press the "Submit" button.'
+            "type": "radiogroup",
+            "name": "Question4",
+            "title": "Feeling tired or having little energy",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
+          }
+        ]
+      },
+      {
+        questions: [
+          {
+            "type": "radiogroup",
+            "name": "Question5",
+            "title": "Poor appetite or overeating",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
+          }
+        ]
+      },
+      {
+        questions: [
+          {
+            "type": "radiogroup",
+            "name": "Question6",
+            "title": "Feeling bad about yourself — or that you are a failure or have let yourself or your family down",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
+          }
+        ]
+      },
+      {
+        questions: [
+          {
+            "type": "radiogroup",
+            "name": "Question7",
+            "title": "Trouble concentrating on things, such as reading the newspaper or watching television.",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
+          }
+        ]
+      },
+      {
+        questions: [
+          {
+            "type": "radiogroup",
+            "name": "Question8",
+            "title": "Moving or speaking so slowly that other people could have noticed? Or the opposite — being so fidgety or restless that you have been moving .around a lot more than usual",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
+          }
+        ]
+      },
+      {
+        questions: [
+          {
+            "type": "radiogroup",
+            "name": "Question9",
+            "title": "Thoughts that you would be better off dead or of hurting yourself in some way",
+            "isRequired": true,
+            "colCount": 0,
+            "choices": ["1|Not At All", "2|Several Days", "3|More the half the days", "4|Nealy"]
           }
         ]
       }
@@ -279,10 +181,6 @@ class NewSurvey extends Component {
     var model = new Survey.Model(this.json);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React with SurveyJS</h2>
-        </div>
         <div className="surveyjs">
           {/*If you want to show survey, uncomment the line below*/}
           <h1>SurveyJS library in action:</h1>
