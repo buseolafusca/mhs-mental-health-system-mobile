97f44e9c30fa68192a8ed2e7df36be6a25cb381f
nickbaf <nickbaf@icloud.com>
2019-07-08 14:43:47 +0100

    code clean up

--- src/variables/general.js
+++ src/variables/general.js
@@ -5,4 +5,6 @@ var fetchQuestionnaireUrl = "/api/v1/questionnaire_sJS/";
 /* HERE MAPS API CREDENTIALS */
 var app_id="nuT8ftiOYvrfFNaFEUyV";
 var app_code="yNZIQaMP6fRuY1D8DKsuxw";
-export {baseUrl,fetchQuestionnaireUrl}
+
+
+export {baseUrl,fetchQuestionnaireUrl,app_code,app_id}
