fa8c487b9b0c4b3104b5e179e3e54fbfc2bd5ce9
Yiming <759832901@qq.com>
2019-07-16 14:56:54 +0100

    fix test issue

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import $ from 'jquery';
+import 'jest-canvas-mock';
 global.$ = global.jQuery = $;
 configure({ adapter: new Adapter() })
