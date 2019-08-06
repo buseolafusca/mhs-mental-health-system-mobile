72b3b7f5e4a927b3467bd2cd9ed5c1f624241028
Yiming <759832901@qq.com>
2019-08-06 12:24:11 +0100

    remove table

--- src/variables/general.js
+++ src/variables/general.js
@@ -5,7 +5,7 @@ var backendUrl = "mhsbackend.azurewebsites.net/api/v1";
 // 'http://178.128.34.125/api/v1'//"
 var createUserAnswers = "/useranswers";
 var patientanswersUrl = "/patientanswers";
-var baseUrl = 'mhsbackend.azurewebsites.net'
+var baseUrl = 'http://178.128.34.125/api/v1'//'mhsbackend.azurewebsites.net'
 var fetchQuestionnaireUrl = '/questionnaire_sJS/'
 var authenticationUrl = "/patients/authenticate";
 var questionnaireWithoutToken = "/questionnaires";
