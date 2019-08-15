fed3be629d51244840cca208def6cadbdbcacc68
nickbaf <nickbaf@icloud.com>
2019-08-15 10:47:38 +0100

    added hint

--- src/layouts/NewSurvey.js
+++ src/layouts/NewSurvey.js
@@ -146,7 +146,7 @@ class NewSurvey extends Component {
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Modal Header</h4>
+                <h4 class="modal-title">Information</h4>
               </div>
               <div class="modal-body">
                 <p><div id="questionDescriptionText"></div></p>
